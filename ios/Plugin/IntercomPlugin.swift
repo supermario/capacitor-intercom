8ef9d068452dd245cfbb0b96534d4331fe96d5e5
Mario Rogic <hello@mario.net.au>
2023-10-31 16:23:32 +0000

    Fix API deprecations in v14

--- ios/Plugin/IntercomPlugin.swift
+++ ios/Plugin/IntercomPlugin.swift
@@ -12,15 +12,15 @@ public class IntercomPlugin: CAPPlugin {
         let apiKey = getConfig().getString("iosApiKey") ?? "ADD_IN_CAPACITOR_CONFIG_JSON"
         let appId = getConfig().getString("iosAppId") ?? "ADD_IN_CAPACITOR_CONFIG_JSON"
         Intercom.setApiKey(apiKey, forAppId: appId)
-        
+
 #if DEBUG
         Intercom.enableLogging()
 #endif
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(self.didRegisterWithToken(notification:)), name: Notification.Name.capacitorDidRegisterForRemoteNotifications, object: nil)
     }
-    
-    
+
+
     @objc func didRegisterWithToken(notification: NSNotification) {
         guard let deviceToken = notification.object as? Data else {
             return
@@ -31,17 +31,17 @@ public class IntercomPlugin: CAPPlugin {
     @objc func loadWithKeys(_ call: CAPPluginCall) {
         let appId = call.getString("appId")  as? String ?? "NO_APP_ID_PASSED"
         let apiKey = call.getString("apiKeyIOS") as? String ?? "NO_API_KEY_PASSED"
-        
+
         Intercom.setApiKey(apiKey, forAppId: appId)
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(self.didRegisterWithToken(notification:)), name: Notification.Name(CAPNotifications.DidRegisterForRemoteNotificationsWithDeviceToken.name()), object: nil)
     }
-    
+
     @objc func registerIdentifiedUser(_ call: CAPPluginCall) {
         let userId = call.getString("userId")
         let email = call.getString("email")
         let attributes = ICMUserAttributes()
-        
+
         if ((email) != nil) {
             attributes.email = email
             Intercom.loginUser(with: attributes) { result in
@@ -51,7 +51,7 @@ public class IntercomPlugin: CAPPlugin {
                 }
             }
         }
-        
+
         if ((userId) != nil) {
             attributes.userId = userId
             Intercom.loginUser(with: attributes) { result in
@@ -62,12 +62,12 @@ public class IntercomPlugin: CAPPlugin {
             }
         }
     }
-    
+
     @objc func registerUnidentifiedUser(_ call: CAPPluginCall) {
         Intercom.loginUnidentifiedUser()
         call.resolve()
     }
-    
+
     @objc func updateUser(_ call: CAPPluginCall) {
         let userAttributes = ICMUserAttributes()
         let userId = call.getString("userId")
@@ -95,31 +95,31 @@ public class IntercomPlugin: CAPPlugin {
         Intercom.updateUser(with: userAttributes)
         call.resolve()
     }
-    
+
     @objc func logout(_ call: CAPPluginCall) {
         Intercom.logout()
         call.resolve()
     }
-    
+
     @objc func logEvent(_ call: CAPPluginCall) {
         let eventName = call.getString("name")
         let metaData = call.getObject("data")
-        
+
         if (eventName != nil && metaData != nil) {
             Intercom.logEvent(withName: eventName!, metaData: metaData!)
-            
+
         }else if (eventName != nil) {
             Intercom.logEvent(withName: eventName!)
         }
-        
+
         call.resolve()
     }
-    
+
     @objc func displayMessenger(_ call: CAPPluginCall) {
-        Intercom.presentMessenger();
+        Intercom.present();
         call.resolve()
     }
-    
+
     @objc func displayMessageComposer(_ call: CAPPluginCall) {
         guard let initialMessage = call.getString("message") else {
             call.reject("Enter an initial message")
@@ -128,49 +128,49 @@ public class IntercomPlugin: CAPPlugin {
         Intercom.presentMessageComposer(initialMessage);
         call.resolve()
     }
-    
+
     @objc func displayHelpCenter(_ call: CAPPluginCall) {
-        Intercom.presentHelpCenter()
+        Intercom.present(Space.helpCenter)
         call.resolve()
     }
-    
+
     @objc func hideMessenger(_ call: CAPPluginCall) {
         Intercom.hide()
         call.resolve()
     }
-    
+
     @objc func displayLauncher(_ call: CAPPluginCall) {
         Intercom.setLauncherVisible(true)
         call.resolve()
     }
-    
+
     @objc func hideLauncher(_ call: CAPPluginCall) {
         Intercom.setLauncherVisible(false)
         call.resolve()
     }
-    
+
     @objc func displayInAppMessages(_ call: CAPPluginCall) {
         Intercom.setInAppMessagesVisible(true)
         call.resolve()
     }
-    
+
     @objc func hideInAppMessages(_ call: CAPPluginCall) {
         Intercom.setInAppMessagesVisible(false)
         call.resolve()
     }
-    
+
     @objc func displayCarousel(_ call: CAPPluginCall) {
         if let carouselId = call.getString("carouselId") {
-            Intercom.presentCarousel(carouselId)
+            Intercom.presentContent(.carousel(id: carouselId))
             call.resolve()
         }else{
             call.reject("carouselId not provided to displayCarousel.")
         }
     }
-    
+
     @objc func setUserHash(_ call: CAPPluginCall) {
         let hmac = call.getString("hmac")
-        
+
         if (hmac != nil) {
             Intercom.setUserHash(hmac!)
             call.resolve()
@@ -179,12 +179,12 @@ public class IntercomPlugin: CAPPlugin {
             call.reject("No hmac found. Read intercom docs and generate it.")
         }
     }
-    
+
     @objc func setBottomPadding(_ call: CAPPluginCall) {
-        
+
         if let value = call.getString("value"),
            let number = NumberFormatter().number(from: value) {
-            
+
             Intercom.setBottomPadding(CGFloat(truncating: number))
             call.resolve()
             print("set bottom padding")
@@ -192,14 +192,13 @@ public class IntercomPlugin: CAPPlugin {
             call.reject("Enter a value for padding bottom")
         }
     }
-    
+
     @objc func displayArticle(_ call: CAPPluginCall) {
         if let articleId = call.getString("articleId") {
-            Intercom.presentArticle(articleId)
+            Intercom.presentContent(.article(id: articleId))
             call.resolve()
         } else {
             call.reject("articleId not provided to presentArticle.")
         }
     }
 }
-
